769cfea662ace9a7592d8828ee86fb50665203a8
Filip <filip.steenbergen@gmail.com>
2023-05-27 15:36:17 +0200

    Employee has comments

--- warehouse/src/Employee.cpp
+++ warehouse/src/Employee.cpp
@@ -3,26 +3,33 @@
 
 Employee::Employee(std::string benaming, bool certificate){
     // const is weggehaald, mocht van david, is sowieso const omdat er geen setter is
+    
+    // variables are being set.
     name = benaming;
     forkliftCertificate = certificate;
 }
 
 std::string Employee::getName(){
+    // return name from employee
     return name;
 };
 
 bool Employee::getBusy(){
+    // gives busy status
     return busy;
 };
 
 void Employee::setBusy(bool bezig){
+    // sets 'busy' status to true
     busy = true;
 };
 
 bool Employee::getForkliftCertificate(){
+    // return if employee has forkliftcertificate or not
     return forkliftCertificate;
 };
 
 void Employee::setForkliftCertificate(bool forkliftcertificate){
+    // sets forkliftcertificate status to true
     forkliftCertificate = true;
 };
